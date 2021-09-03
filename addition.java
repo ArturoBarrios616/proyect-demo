c96c90c40861aa3e0e836067c3d2bf411e7e7938
Weebmaster666 <A0173269@itesm.mx>
2021-09-03 10:18:20 -0500

    code y nombr de autor

--- addition.java
+++ addition.java
@@ -1,1 +1,10 @@
 //insert code here
+public class IntegerSumExample1 {
+public static void main(String[] args) {
+int a = 65;
+int b = 35;
+// It will return the sum of a and b.
+System.out.println("The sum of a and b is = " + Integer.sum(a, b));
+}
+}
+//by weebmastter666
